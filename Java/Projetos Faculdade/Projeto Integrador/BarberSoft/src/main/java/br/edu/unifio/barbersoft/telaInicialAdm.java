c0539f6b669a30640b5497e4cfd5f5e0f07c4a3c
RaphaelAml <raphaelaml1@outlook.com>
2023-05-01 17:40:14 -0300

    Adicionado botao agendamento

--- Java/Projetos Faculdade/Projeto Integrador/BarberSoft/src/main/java/br/edu/unifio/barbersoft/telaInicialAdm.java
+++ Java/Projetos Faculdade/Projeto Integrador/BarberSoft/src/main/java/br/edu/unifio/barbersoft/telaInicialAdm.java
@@ -26,21 +26,42 @@ public class telaInicialAdm extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        btnAgendamento = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        btnAgendamento.setText("AGENDAMENTO");
+        btnAgendamento.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAgendamentoActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(53, 53, 53)
+                .addComponent(btnAgendamento, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(497, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(69, 69, 69)
+                .addComponent(btnAgendamento, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(347, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnAgendamentoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgendamentoActionPerformed
+      
+        telaAgendamentoAdm telaAgendamentoAdm = new telaAgendamentoAdm();
+        setVisible(true);
+    }//GEN-LAST:event_btnAgendamentoActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +99,6 @@ public class telaInicialAdm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAgendamento;
     // End of variables declaration//GEN-END:variables
 }
