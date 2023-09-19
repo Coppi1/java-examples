44291113a4e8bdfaf202295f2bb80a9b0218cac9
Eduardo Coppi <educoppi00@gmail.com>
2023-09-19 20:03:39 -0300

    Add Exercícios

---
+++ Java/4 termo/Exercicios/exHibernateJPA/src/main/java/br/com/exhibernatejpa/AplicacaoMain.java
@@ -0,0 +1,69 @@
+package br.com.exhibernatejpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+import model.Cliente;
+
+import java.util.List;
+
+public class AplicacaoMain {
+    public static void main(String[] args) {
+        System.out.println("Iniciando APP");
+        Cliente c = new Cliente();
+        c.setNome("jorgin");
+        c.setEmail("teste2@gmail.com");
+        //Declarando persistence
+        EntityManagerFactory entityManFac = Persistence.createEntityManagerFactory("default");
+        //usando o persistence
+        EntityManager entityManager = entityManFac.createEntityManager();
+        // Iniciando a transação com o banco de dados
+        entityManager.getTransaction().begin();
+
+        // Persistindo (gravando) o objeto c no banco de dados
+        //entityManager.persist(c);
+
+        //Fazendo busca no BD
+        //Cliente clibusca = entityManager.find(Cliente.class, 2);
+
+
+        //System.out.println("Cliente encontrado: " + clibusca.getNome());
+
+        // alterando dados do clibusca (cliente id 2)
+        //clibusca.setNome("Jorgin 2.0");
+
+        //excluindo o cliente id 2 clibusca
+       // entityManager.remove(clibusca);
+
+        // "comitando" (efetivando/enviando) a atualização no BD
+        //entityManager.getTransaction().commit();
+
+        //variável para utilizar na query
+        String busca = "j";
+
+        //Usando Query SQL
+        Query q1 = entityManager.createQuery("SELECT e FROM Cliente e " +
+                "WHERE e.nome " +
+                "LIKE :ParametroNome");
+        //trocando o parametro da query, pela variável busca
+        q1.setParameter("ParametroNome",  busca + "%");
+
+        //Armazenando resultado em arraylist do tipo entidade Cliente
+        List<Cliente> listaResultado = q1.getResultList();
+
+        //exibindo array, utilizando a quantidade de registro no objeto 'e'
+        for (Cliente e:listaResultado){
+            System.out.println("Nome: " + e.getNome() + "\nEmail: " + e.getEmail());
+        }
+
+
+
+
+
+        // Finalizando as comunicações com BD
+        entityManager.close();
+        entityManFac.close();
+
+    }
+}
---
+++ Java/4 termo/Exercicios/helloWorld/src/main/java/br/com/helloworld/Repositorios/produtoRepositorio.java
@@ -0,0 +1,24 @@
+package br.com.helloworld.Repositorios;
+
+import br.com.helloworld.Entidades.Produto;
+import jakarta.faces.view.ViewScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@ViewScoped
+@Component
+public class produtoRepositorio {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional  // serve para editar, inserir e excluir, não para select
+    public void inserir(Produto produto){
+
+        entityManager.persist(produto);
+
+    }
+}
