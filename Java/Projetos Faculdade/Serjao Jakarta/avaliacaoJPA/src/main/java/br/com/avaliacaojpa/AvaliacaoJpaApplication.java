378746c47fdc5cd06027e500dd7ea44f70552ed2
Eduardo Coppi <educoppi00@gmail.com>
2023-09-23 11:12:18 -0300

    AvaliacaoJPA ADD

---
+++ Java/Projetos Faculdade/Serjao Jakarta/avaliacaoJPA/src/main/java/br/com/avaliacaojpa/AvaliacaoJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.avaliacaojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoJpaApplication.class, args);
+	}
+
+}
---
+++ Java/Projetos Faculdade/Serjao Jakarta/avaliacaoJPA/src/test/java/br/com/avaliacaojpa/AvaliacaoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.avaliacaojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
